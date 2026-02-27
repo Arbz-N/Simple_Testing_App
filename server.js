5db0218e640227f4b3d7170907ff4f84b448b01a
Arbaz Naeem <arbaznaeem555@gmail.com>
2026-02-28 03:53:34 +0500

    added node.js base application

---
+++ server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  fs.createReadStream(path.join(__dirname, 'index.html')).pipe(res);
+});
+
+server.listen(PORT, () => {
+  console.log(`Production server running on port ${PORT}`);
+});
